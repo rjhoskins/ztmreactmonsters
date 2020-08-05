fc42245dc4dab46d061945a0d795c27200e47a79
Rob Hoskins <robertjhoskins@gmail.com>
2020-08-04 22:10:15 -0700

    Updates

---
+++ precache-manifest.e687a6722c76689895d64aac4e46d121.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742ab3c747a1117486ae875b0230a6a4",
+    "url": "/ztmreactmonsters/index.html"
+  },
+  {
+    "revision": "6f6ef09ac447b76aebda",
+    "url": "/ztmreactmonsters/static/css/main.c8082f50.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/ztmreactmonsters/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ztmreactmonsters/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f6ef09ac447b76aebda",
+    "url": "/ztmreactmonsters/static/js/main.bb546962.chunk.js"
+  },
+  {
+    "revision": "a744b43597c9f31855cb",
+    "url": "/ztmreactmonsters/static/js/runtime-main.5e731e56.js"
+  }
+]);
